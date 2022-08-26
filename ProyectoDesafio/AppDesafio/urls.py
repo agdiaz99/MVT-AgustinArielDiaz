c67cb60dbb5666cff87f54686da88886bd98a6f1
Agustin A. Diaz <agustin.d2013@gmail.com>
2022-08-26 12:10:50 -0300

    Familiares creados en views.py con models.py (class Persona) importada y se muestra en pantalla

--- ProyectoDesafio/AppDesafio/urls.py
+++ ProyectoDesafio/AppDesafio/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from AppDesafio.views import template, inicio, segunda_view, dia_hoy, nombre, ano_nacimiento
+from AppDesafio.views import *
 
 urlpatterns = [
     path('', template, name='template'),
     path('inicio/', inicio, name='inicio'),
-    path('segundaview/', segunda_view, name='segunda_view'),
+    path('nosotros/', nosotros, name='nosotros'),
+    path('servicios/', servicios, name='servicios'),
+    path('clases/', clases, name='clases'),
+    path('single/', single, name='single'),
+    path('blog/', blog, name='blog'),
+    path('contacto/', contacto, name='contacto'),
     path('diahoy/', dia_hoy, name='dia_hoy'),
     path('nombre/<nombre>', nombre, name='nombre'),
     path('anonacimiento/<ano>', ano_nacimiento, name='ano_nacimiento'),
+    path('familiar1/', familiar1, name='familiar1'),
+    path('familiar2/', familiar2, name='familiar2'),
+    path('familiar3/', familiar3, name='familiar3'),
 ]
--- ProyectoDesafio/AppDesafio/views.py
+++ ProyectoDesafio/AppDesafio/views.py
@@ -5,14 +5,59 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 from django.template import loader
+from .models import Persona
+
+
+def familiar1(request):
+    
+    familiar1 = Persona(nombre="Marcos", apellido="Diaz", edad="25", ano_nacimiento="1996")
+    familiar1.save()
+    texto=f"Mi nombre es {familiar1.nombre} {familiar1.apellido}, tengo {familiar1.edad} y nací en {familiar1.ano_nacimiento}"
+    return HttpResponse(texto)
+
+
+def familiar2(request):
+    
+    familiar2 = Persona(nombre="Sergio", apellido="Chiaradio", edad="24", ano_nacimiento="1997")
+    familiar2.save()
+    texto=f"Mi nombre es {familiar2.nombre} {familiar2.apellido}, tengo {familiar2.edad} y nací en {familiar2.ano_nacimiento}"
+    return HttpResponse(texto)
+
+
+def familiar3(request):
+    
+    familiar3 = Persona(nombre="Facundo", apellido="Martinez", edad="23", ano_nacimiento="1998")
+    familiar3.save()
+    texto=f"Mi nombre es {familiar3.nombre} {familiar3.apellido}, tengo {familiar3.edad} y nací en {familiar3.ano_nacimiento}"
+    return HttpResponse(texto)
 
 
 def inicio(request):
-    return render(request, "AppDesafio/inicio.html")
+    return render(request, "AppDesafio/index.html")
 
 
-def segunda_view(request):
-    return render(request, "AppDesafio/template1.html")
+def nosotros(request):
+    return render(request, "AppDesafio/about.html")
+
+
+def servicios(request):
+    return render(request, "AppDesafio/feature.html")
+
+
+def clases(request):
+    return render(request, "AppDesafio/class.html")
+
+
+def single(request):
+    return render(request, "AppDesafio/single.html")
+
+
+def blog(request):
+    return render(request, "AppDesafio/blog.html")
+
+
+def contacto(request):
+    return render(request, "AppDesafio/contact.html")
 
 
 def dia_hoy(request):
